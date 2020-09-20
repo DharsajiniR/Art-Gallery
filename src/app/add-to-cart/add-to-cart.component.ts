fc24779883a5daf7ce26e8382464f6252e5e10c3
antony-8610 <antony.r@zohocorp.com>
2020-09-20 17:55:44 +0530

    style changes to add cart section

--- src/app/add-to-cart/add-to-cart.component.ts
+++ src/app/add-to-cart/add-to-cart.component.ts
@@ -13,40 +13,50 @@ export class AddToCartComponent implements OnInit {
   public  cartproducts: Product[] = [];
   public imageWidth = 250;
   public imageMargin = 2;
-  public total: number=0;
+  public total: number = 0;
+  public totalItemsInCartDesc;
+
    constructor(private productService: ProductService,private router : Router) { }
- 
+
    ngOnInit(): void {
      this.productService.getProducts().subscribe({
        next: products => {
-         this.cartproducts = products;
-         this.cartproducts.forEach(element=>{
-          element.quantity=1;
-          element.totalPrice=element.price;
-          this.total+=element.totalPrice;
+          this.cartproducts = products;
+          const totalItemsInCart = this.cartproducts.length;
+
+          if (totalItemsInCart === 0) {
+            this.totalItemsInCartDesc = 'No items'
+          } else if (totalItemsInCart === 1){
+            this.totalItemsInCartDesc = '1 item';
+          } else {
+            this.totalItemsInCartDesc = totalItemsInCart + ' items';
+          }
+
+          this.cartproducts.forEach(element => {
+          element.quantity = 1;
+          element.totalPrice = element.price;
+          this.total += element.totalPrice;
          });
        }
      });
    }
 
    public qtyDecrease(item : Product) {
-    if(item.quantity>1){
+    if (item.quantity > 1) {
      item.quantity--;
-     item.totalPrice=item.price*item.quantity;
-     this.total=this.total-item.price;
+     item.totalPrice = item.price * item.quantity;
+     this.total -= item.price;
     }
    }
 
    public qtyIncrease(item : Product) {
     item.quantity++;
-    item.totalPrice=item.price*item.quantity;
-    this.total+=item.price;
+    item.totalPrice = item.price * item.quantity;
+    this.total += item.price;
   }
 
   public deleteItem(index : number) {
-   this.cartproducts.splice(index,1);
+   this.cartproducts.splice(index, 1);
   }
-
-
 
 }
